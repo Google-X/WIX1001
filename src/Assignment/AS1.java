10db1e3f306dd747233649334b9e19309aa1eb39
Google-X <i.code.like.crazy.everyday@gmail.com>
2019-11-19 07:51:24 +0800

    AS1 Correction Final Update @ 191119, 07:51

--- src/Assignment/AS1.java
+++ src/Assignment/AS1.java
@@ -3,7 +3,10 @@
 1. Consider all permutations of the numbers 1 to n. A good permutation is one where for any number i at position p in the permutation, i+1 is never at position p+1. For a given n, count the number of good permutations.
 
 TIPS:
-a(n) = n * a(n-1) + (n-1) * a(n-2)
+ORIGINAL: a(n+1) = na(n) + (n−1)a(n−1)
+a(n) = n * a(n-1) + (n-1) * a(n-2) << Wrong
+
+f(n) = (n-1)*f(n-1) + (n-2)*f(n-2)
 The number of "good permutation"
 n   number of good permutation
 2   1
@@ -24,16 +27,16 @@ public class AS1 {
     
     public static int re(int n){
         
-        if(n == 0 || n == 1)
+        if(n == 0 || n == 1 || n == 2)
             return 1;
         else
-            return n * re(n-1) + (n-1) * re(n-2);
+            return (n-1) * re(n-1) + (n-2) * re(n-2);
     }
     
     public static void main(String[] args) {
         
-        for(int i = 0; i < 9; i++){
-            System.out.println(i+1 + " : " + re(i));       
+        for(int i = 1; i < 10; i++){
+            System.out.println(i + " : " + re(i));       
         }   
     }
 }
